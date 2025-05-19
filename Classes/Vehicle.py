ed8f2a0aa927a19f474ff96f769be0c51513be27
josecmarinho <up202403880@up.pt>
2025-05-19 20:42:18 +0100

    sales now works

--- Classes/Vehicle.py
+++ Classes/Vehicle.py
@@ -23,6 +23,7 @@ class vehicle(Gclass):
         self._model = model
         self._make = make
         self._price = float(price)
+        
         vehicle.obj[id] = self
         vehicle.lst.append(id)
     
